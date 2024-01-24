f4c4664e66dda85b4b58c966e33a4cd543b7d640
alexandr <matcnbev2001@mail.ru>
2024-01-24 12:52:08 +0400

    ref

--- iternal/api/user_saver.go
+++ iternal/api/user_saver.go
@@ -4,23 +4,43 @@ import (
 	"name_service/iternal/models"
 	rep "name_service/iternal/repository"
 	api "name_service/pkg/api/privider"
+	"sync"
 )
 
 func Save(userData *models.UserData) (*models.UserData, error) {
 	name := userData.Name
 
-	age, err := api.GetAge(name)
-	exceptions(err)
+	var wg sync.WaitGroup
+	wg.Add(3)
 
-	gender, err := api.GetGender(name)
-	exceptions(err)
+	go func() {
+		age, err := api.GetAge(name)
+		if err != nil {
+			exceptions(err)
+		}
+		userData.Age = age
+		wg.Done()
+	}()
 
-	nationality, err := api.GetNationality(name)
-	exceptions(err)
+	go func() {
+		gender, err := api.GetGender(name)
+		if err != nil {
+			exceptions(err)
+		}
+		userData.Gender = gender
+		wg.Done()
+	}()
 
-	userData.Age = age
-	userData.Gender = gender
-	userData.Nationality = nationality[0]
+	go func() {
+		nationality, err := api.GetNationality(name)
+		if err != nil {
+			exceptions(err)
+		}
+		userData.Nationality = nationality[0]
+		wg.Done()
+	}()
+
+	wg.Wait()
 
 	return rep.SaveUserData(userData)
 }
